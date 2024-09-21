9b7ebb6485f0eba92630e05b00eb1174f85b733b
ffelixg <142172984+ffelixg@users.noreply.github.com>
2024-09-21 18:54:11 +0200

    Split off binary output, add doc strings

--- import_zig/__init__.py
+++ import_zig/__init__.py
@@ -6,23 +6,37 @@ import sysconfig
 import sys
 import subprocess
 import random
-import typing
 
-copy_paths = [
+_copy_paths = [
     Path(__file__).parent / "build.zig",
     Path(__file__).parent / "py_utils.zig",
     Path(__file__).parent / "zig_ext.zig",
 ]
 
 
-def _escape(path: str):
+def _escape(path: str) -> str:
     return path.replace("\\", "\\\\")
 
 
-def prepare(path: Path, module_name: str):
-    "Link/Create files at path needed to compile the Zig code"
-    for cp in copy_paths:
-        (path / cp.name).symlink_to(cp)
+def prepare(path: str | Path, module_name: str, symlink_only: bool = False):
+    """
+    Link/Create files at path needed to compile the Zig code
+
+    In order to get ZLS support for the Python C API, you can execute this and
+    develop inside the "inner" directory.
+    """
+    path = Path(path)
+    if not path.exists():
+        path.mkdir()
+
+    for src in _copy_paths:
+        tgt = path / src.name
+        if symlink_only:
+            (tgt).symlink_to(src)
+        else:
+            copyfile(src, tgt)
+
+    (path / "inner").mkdir()
 
     include_dirs = [sysconfig.get_path("include")]
     lib_paths = [
@@ -41,49 +55,93 @@ def prepare(path: Path, module_name: str):
         )
 
 
-def import_zig(
+def compile_to(
+    target_dir: str | Path,
+    module_name: str = "zig_ext",
     source_code: str | None = None,
     file: Path | str | None = None,
     directory: Path | str | None = None,
-    module_name: str | None = None,
-    return_action: typing.Literal["import", "binary_path"] = "import",
 ):
-    if (source_code is not None) + (file is not None) + (directory is not None) != 1:
-        raise Exception("Can only use one method to specify location of Zig file(s).")
-    if module_name is None:
-        module_name = f"zig_ext_{hex(random.randint(0, 2**128))[2:]}"
+    """
+    Same as import_zig, except that the module will not be imported an instead
+    copied into the directory specified by `path_target`.
 
-    with TemporaryDirectory(prefix="import_zig_") as tempdir:
+    Further, `module_name` is not randomized.
+    """
+    if (source_code is not None) + (file is not None) + (directory is not None) != 1:
+        raise Exception(
+            "Exactly one method must be used to specify location of Zig file(s)."
+        )
+
+    with TemporaryDirectory(prefix="import_zig_compile_") as tempdir:
         temppath = Path(tempdir)
-        prepare(temppath, module_name)
+        prepare(temppath, module_name, symlink_only=True)
 
         temppath_inner = temppath / "inner"
         if directory is not None:
+            temppath_inner.rmdir()
             temppath_inner.symlink_to(Path(directory).absolute())
+        elif file is not None:
+            (temppath_inner / "import_fns.zig").symlink_to(Path(file).absolute())
         else:
-            temppath_inner.mkdir()
-            if file is not None:
-                (temppath_inner / "import_fns.zig").symlink_to(Path(file).absolute())
-            else:
-                assert source_code is not None
-                with (temppath_inner / "import_fns.zig").open("w") as f:
-                    f.write(source_code)
+            assert source_code is not None
+            with (temppath_inner / "import_fns.zig").open("w") as f:
+                f.write(source_code)
 
         args = [sys.executable, "-m", "ziglang", "build"]
         subprocess.run(args, cwd=tempdir, check=True)
 
         (binary,) = (p for p in (temppath / "zig-out").glob("**/*") if p.is_file())
-        new_binary = temppath / (module_name + sysconfig.get_config_var("EXT_SUFFIX"))
-        binary.rename(new_binary)
+        binary.rename(
+            Path(target_dir) / (module_name + sysconfig.get_config_var("EXT_SUFFIX"))
+        )
 
-        if return_action == "import":
-            sys.path.append(tempdir)
+
+def import_zig(
+    module_name: str | None = None,
+    source_code: str | None = None,
+    file: Path | str | None = None,
+    directory: Path | str | None = None,
+):
+    """
+    This function takes in Zig code, wraps it in the Python C API, compiles the
+    code and returns the imported binary.
+
+    Assumptions on the code:
+    The Zig source can be specified as a source code string, a file or a directory.
+    If it is specified as a directory, the file containin the functions which get
+    exported to Python must be named `import_fns.zig`, however that file may use
+    any other files present in the directory.
+
+    A function gets exposed to Python if it is marked pub.
+
+    It is possible to use
+    ```
+    const pyu = @import("../py_utils.zig");
+    const py = pyu.py;
+    ```
+    in order to access the Python C API with `py` and utilities with `pyu`. This
+    allows for example raising exceptions or passing Python objects with
+    `*py.PyObject`.
+
+    If module_name is left blank, a random name will be assigned.
+    """
+    if module_name is None:
+        module_name = f"zig_ext_{hex(random.randint(0, 2**128))[2:]}"
+
+    with TemporaryDirectory(prefix="import_zig_") as tempdir:
+        compile_to(
+            tempdir,
+            source_code=source_code,
+            file=file,
+            directory=directory,
+            module_name=module_name,
+        )
+        sys.path.append(tempdir)
+        try:
             module = import_module(module_name)
+        finally:
             sys.path.remove(tempdir)
-            return module
-        elif return_action == "binary_path":
-            return_binary = new_binary.parent.parent / new_binary.name
-            new_binary.rename(return_binary)
-            return return_binary
-        else:
-            raise Exception("Invalid return_action")
+        return module
+
+
